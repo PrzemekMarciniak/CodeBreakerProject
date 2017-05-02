4395f3aff18fe24e31f6720e5441da4c70d39943
Przemek Marciniak <przemek@marciniak.vc>
2017-05-02 11:21:18 +0200

    Add guess function

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,14 +3,19 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    
+    if (answer == "" || attempt == "") {
+    	setHiddenFields ();
 }
 
-//implement new functions here
+
 
 function setHiddenFields () {
+	
 	answer.value = Math.floor(Math.random() * 10000).toString();
 	while (answer.value.length < 4) {
 		answer.value = "0" + answer.value;
 	}
+		attempt.value = "0";
+
 }
