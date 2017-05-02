d971b605760b49146151355886eabf1025db9189
Przemek Marciniak <przemek@marciniak.vc>
2017-05-02 11:12:28 +0200

    Loop check number length

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,5 +9,8 @@ function guess() {
 //implement new functions here
 
 function setHiddenFields () {
-	answear.value = Math.floor(Math.random() * 10000);
+	answer.value = Math.floor(Math.random() * 10000).toString();
+	while (answer.value.length < 4) {
+		answer.value = "0" + answer.value;
+	}
 }
